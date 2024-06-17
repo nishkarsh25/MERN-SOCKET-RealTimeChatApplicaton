748f81cd8c9ccf2cc8ee974a576085a6435c85f8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 08:50:27 +0530

    Import Statements Importing Express:
    import express from "express";: Imports the express library which is used to create a router for handling routes related to user operations.
    Importing Controllers:
    
    import { getOtherUsers, login, logout, register } from "../controllers/userController.js";: Imports the controller functions (getOtherUsers, login, logout, register) from the userController.js file. These functions handle the actual logic for each route.
    Importing Middleware:
    
    import isAuthenticated from "../middleware/isAuthenticated.js";: Imports the isAuthenticated middleware from the specified path. This middleware is used to protect routes by ensuring that the user is authenticated before accessing them.

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getOtherUsers, login, logout, register } from "../controllers/userController.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
+
+
+
+
+
