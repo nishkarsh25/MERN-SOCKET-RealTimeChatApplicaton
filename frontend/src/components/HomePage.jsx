dfca8dfeb73c870e74236fe278d8978ecde29852
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:19:48 +0530

    Imports React and Hooks:
    React: The core library for building the user interface.
    useEffect: A hook that runs side effects in functional components.
    Components:
    
    Sidebar: A component that likely contains navigation or other sidebar content.
    MessageContainer: A component that likely displays messages or main content.
    Redux Hooks:
    
    useSelector: A hook from react-redux to access the Redux store's state.
    React Router:
    
    useNavigate: A hook from react-router-dom used for programmatic navigation.

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react'
+import Sidebar from './Sidebar'
+import MessageContainer from './MessageContainer'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+
