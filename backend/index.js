70818bc4b6e461596ac7f6c8c1e53e8f4137d346
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:01:42 +0530

    Import Statements Importing Modules: import express from "express";: Imports the express module. This is the modern ES module syntax for importing modules in JavaScript. import dotenv from "dotenv";: Imports the dotenv module, which loads environment variables from a .env file into process.env. import connectDB from "./config/database.js";: Imports the connectDB function from the database.js file located in the config directory. import userRoute from "./routes/userRoute.js";: Imports the userRoute router from the userRoute.js file located in the routes directory. import messageRoute from "./routes/messageRoute.js";: Imports the messageRoute router from the messageRoute.js file located in the routes directory. import cookieParser from "cookie-parser";: Imports the cookie-parser middleware, which parses cookies attached to the client request object. import cors from "cors";: Imports the cors middleware, which enables Cross-Origin Resource Sharing (CORS) configuration in the application. import { app, server } from "./socket/socket.js";: Imports the app and server objects from the socket.js file located in the socket directory.

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+// const express = require('express')// method-1
+import express from "express"; // method-2
+import dotenv from "dotenv"; 
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app,server } from "./socket/socket.js";
+
+
+ 
+
+
+
+
+
+
+ 
+
+
+
