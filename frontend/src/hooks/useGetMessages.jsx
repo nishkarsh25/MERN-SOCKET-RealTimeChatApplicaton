1647e7f22b581e7f37b4a5d8a854e84f6c940170
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:26:13 +0530

    Imports and Dependencies Imports: import React, { useEffect } from 'react': Imports React and useEffect hook from React library. import axios from "axios";: Imports Axios library for making HTTP requests. import { useSelector, useDispatch } from "react-redux";: Imports useSelector and useDispatch hooks from react-redux for accessing Redux store state and dispatching actions. import { setMessages } from '../redux/messageSlice';: Imports the setMessages action creator from the Redux message slice. import { BASE_URL } from '..';: Imports the BASE_URL constant defined elsewhere in the application.

---
+++ frontend/src/hooks/useGetMessages.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react'
+import axios from "axios";
+import {useSelector,useDispatch} from "react-redux";
+import { setMessages } from '../redux/messageSlice';
+import { BASE_URL } from '..';
+
+
+
