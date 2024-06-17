a1396f063e152bd03d57671004a42c5f7c210246
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:23:20 +0530

    Imports React and Hooks:
    React: The core library for building the user interface.
    useState: A hook to manage local state in functional components.
    Icons:
    
    BiSearchAlt2: An icon from the react-icons library, specifically the BiSearchAlt2 icon for search functionality.
    Components:
    
    OtherUsers: A component that likely displays a list of other users.
    Axios:
    
    A library for making HTTP requests.
    Toast Notifications:
    
    toast from react-hot-toast to display success or error messages.
    React Router:
    
    useNavigate: A hook from react-router-dom for programmatic navigation.
    Redux Hooks and Actions:
    
    useSelector and useDispatch from react-redux to interact with the Redux store.
    setAuthUser, setOtherUsers, setSelectedUser from userSlice.
    setMessages from messageSlice.
    Constants:
    
    BASE_URL: The base URL for making API requests.

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { BiSearchAlt2 } from "react-icons/bi";
+import OtherUsers from './OtherUsers';
+import axios from "axios";
+import toast from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
+import { setAuthUser, setOtherUsers, setSelectedUser } from '../redux/userSlice';
+import { setMessages } from '../redux/messageSlice';
+import { BASE_URL } from '..';
+ 
+
+
