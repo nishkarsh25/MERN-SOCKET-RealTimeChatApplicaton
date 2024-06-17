15457dc34c5948602e87ffd0baa209acef15c6d2
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:19:58 +0530

    Component Definition The HomePage component is a functional component that serves as the main page for authenticated users.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,5 +4,9 @@ import MessageContainer from './MessageContainer'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-
+const HomePage = () => {
+  
+  
+  
+}
 
