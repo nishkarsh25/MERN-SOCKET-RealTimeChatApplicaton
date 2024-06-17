6f37b8f0a33411f0cc8115888251e93de9e5d329
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:16:55 +0530

    Reducer Function Reducer Function: Inside the reducers property of createSlice, each key-value pair represents a Redux action creator function and its corresponding reducer. setSocket: Action creator: setSocket Reducer function: Updates state.socket with action.payload, which is provided when the action is dispatched.

--- frontend/src/redux/socketSlice.js
+++ frontend/src/redux/socketSlice.js
@@ -1,3 +1,13 @@
 import {createSlice} from "@reduxjs/toolkit";
-const socketSlice = createSlice();
+const socketSlice = createSlice({
+    name:"socket",
+    initialState:{
+        socket:null
+    },
+    reducers:{
+        setSocket:(state, action)=>{
+            state.socket = action.payload;
+        }
+    }
+});
 
