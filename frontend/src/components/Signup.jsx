3dbe179d72c0586e724ed20b5f40fea9a156179f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:14:35 +0530

    Imports React and Hooks: React: The core library for building the user interface. useState: A hook for managing local state within the component. useNavigate: A hook from react-router-dom used for navigation. Axios: A promise-based HTTP client used for making HTTP requests. toast from react-hot-toast: A library used for displaying notifications. BASE_URL: A constant that holds the base URL for API requests.

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import toast from "react-hot-toast";
+import { BASE_URL } from '..';
+
+
+
