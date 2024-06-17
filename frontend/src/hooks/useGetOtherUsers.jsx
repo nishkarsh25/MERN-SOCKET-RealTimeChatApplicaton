7526f52fdad13ca254d309796f5233dd7da1c181
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:28:36 +0530

    Imports and Dependencies Imports: import React, { useEffect } from 'react';: Imports React and useEffect hook from React library. import axios from "axios";: Imports Axios library for making HTTP requests. import { useDispatch } from "react-redux";: Imports useDispatch hook from react-redux for accessing the Redux store's dispatch function. import { setOtherUsers } from '../redux/userSlice';: Imports the setOtherUsers action creator from the Redux user slice. import { BASE_URL } from '..';: Imports the BASE_URL constant defined elsewhere in the application.

---
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setOtherUsers } from '../redux/userSlice';
+import { BASE_URL } from '..';
+
+
+
