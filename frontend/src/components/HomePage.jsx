25fd5db1c53f4cd7bacda4b23018589b2b2e079e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:20:08 +0530

    Redux State authUser: This state is accessed from the Redux store to check if the user is authenticated. Navigation navigate: Used to navigate the user to the login page if they are not authenticated.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,7 +5,8 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
-  
+  const { authUser } = useSelector(store => store.user);
+  const navigate = useNavigate();
   
   
 }
