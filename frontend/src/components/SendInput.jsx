5bd6014dc8f2ba90de31ac358549d68cd11ffe94
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:41:20 +0530

    Imports React: Core library for building the user interface. IoSend: Icon component from react-icons/io5, used for displaying a send icon. axios: Library for making HTTP requests. useDispatch: Hook from react-redux used to dispatch actions to the Redux store. useSelector: Hook from react-redux used to access state from the Redux store. setMessages: Action creator imported from ../redux/messageSlice, used to update the messages state. BASE_URL: Constant imported from .., typically containing the base URL of your backend API.

---
+++ frontend/src/components/SendInput.jsx
@@ -0,0 +1,9 @@
+import React, {useState } from 'react'
+import { IoSend } from "react-icons/io5";
+import axios from "axios";
+import {useDispatch,useSelector} from "react-redux";
+import { setMessages } from '../redux/messageSlice';
+import { BASE_URL } from '..';
+
+
+
