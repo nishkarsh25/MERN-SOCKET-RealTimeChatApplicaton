448132cfed7ab5297484a6c8eaa7fbc90eed24fa
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:04:59 +0530

    HTTP Server Setup Creating an HTTP Server: const server = http.createServer(app);: Creates an HTTP server instance using the Express application (app) as its request handler. Socket.IO Setup Creating a Socket.IO Server: const io = new Server(server, { ... });: Creates a new instance of Server from Socket.IO, passing in the server created with http.createServer(). The cors option in the configuration object allows specifying CORS settings for Socket.IO connections. cors: { origin: ['http://localhost:3000'], methods: ['GET', 'POST'] }: Specifies that connections from http://localhost:3000 are allowed, and only GET and POST methods are permitted.

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,7 +4,13 @@ import express from "express";
 
 const app = express();
 
-
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors:{
+        origin:['http://localhost:3000'],
+        methods:['GET', 'POST'],
+    },
+});
 
 
 
