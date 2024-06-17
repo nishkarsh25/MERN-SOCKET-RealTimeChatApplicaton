d8c2d45a064ae5b3670e4cefc127cd492f04f75c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 08:53:44 +0530

    Import Statements Importing Models:
    import { Conversation } from "../models/conversationModel.js";: Imports the Conversation model from the specified path. This model represents the conversations between users.
    import { Message } from "../models/messageModel.js";: Imports the Message model from the specified path. This model represents individual messages sent between users.
    Importing Socket Functions:
    
    import { getReceiverSocketId, io } from "../socket/socket.js";: Imports getReceiverSocketId and io from the specified socket module. These are used for real-time communication with clients via WebSocket.

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,5 @@
+import { Conversation } from "../models/conversationModel.js";
+import { Message } from "../models/messageModel.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
+
