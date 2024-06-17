5229a18a4db4c25f747de04beae8daaa272ade09
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:20:48 +0530

    Persist Configuration Persist Configuration: const persistConfig = { ... };: Defines a persistConfig object: key: Specifies the key to use for storing the persisted state ('root' in this case). version: Specifies the version of the persisted state. storage: Specifies the storage engine to use (storage imported from redux-persist/lib/storage).

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -13,7 +13,11 @@ import {
   } from 'redux-persist';
   import storage from 'redux-persist/lib/storage'
 
-  
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
 
   
 
