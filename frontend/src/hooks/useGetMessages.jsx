d4df060f143bb4353a3ce4b5ce9173896803b512
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:26:53 +0530

    useEffect Hook useEffect Hook: useEffect(() => { ... }, [selectedUser?._id, setMessages]);: Uses the useEffect hook to perform side effects in a functional component. The effect runs whenever selectedUser?._id or setMessages changes.

--- frontend/src/hooks/useGetMessages.jsx
+++ frontend/src/hooks/useGetMessages.jsx
@@ -7,6 +7,17 @@ import { BASE_URL } from '..';
 const useGetMessages = () => {
     const {selectedUser} = useSelector(store=>store.user);
     const dispatch = useDispatch();
-    
+    useEffect(() => {
+        const fetchMessages = async () => {
+            try {
+                axios.defaults.withCredentials = true;
+                const res = await axios.get(`${BASE_URL}/api/v1/message/${selectedUser?._id}`);
+                dispatch(setMessages(res.data))
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchMessages();
+    }, [selectedUser?._id,setMessages]);
 }
 
