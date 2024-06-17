e994250da23e94b7ff9c41bc6329b417724da1aa
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:30:20 +0530

    Imports React and useEffect:
    React is the core library for building user interfaces.
    useEffect is a hook used for side effects, though it is not utilized in this particular code snippet.
    SendInput and Messages:
    
    These are custom components used within MessageContainer for sending and displaying messages.
    useSelector and useDispatch from react-redux:
    
    useSelector is used to access specific parts of the Redux store's state.
    useDispatch is used to dispatch actions to the Redux store.
    setSelectedUser:
    
    An action creator imported from ../redux/userSlice to set the selected user in the Redux store.

---
+++ frontend/src/components/MessageContainer.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react'
+import SendInput from './SendInput'
+import Messages from './Messages';
+import { useSelector,useDispatch } from "react-redux";
+import { setSelectedUser } from '../redux/userSlice';
+
+
