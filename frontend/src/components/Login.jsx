3f991dba9c6ea1044eaff0b23fc4dbcb0e6001b7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:17:33 +0530

    Imports React and Hooks:
    React: The core library for building the user interface.
    useState: A hook for managing local state within the component.
    useNavigate: A hook from react-router-dom used for navigation.
    React Hot Toast:
    
    toast: A library used for displaying notifications.
    Axios:
    
    A promise-based HTTP client used for making HTTP requests.
    Redux Hooks:
    
    useDispatch: A hook from react-redux used to dispatch actions to the Redux store.
    Redux Slice Action:
    
    setAuthUser: An action from the userSlice used to set the authenticated user in the Redux store.
    Constants:
    
    BASE_URL: A constant that holds the base URL for API requests.

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import toast from "react-hot-toast"
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setAuthUser } from '../redux/userSlice';
+import { BASE_URL } from '..';
+
+
