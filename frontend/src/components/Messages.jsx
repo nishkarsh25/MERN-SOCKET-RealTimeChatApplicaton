719a623f2b1666522985dbe1874eb93a22ee0e57
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:39:48 +0530

    Imports React: Core library for building the user interface. Message: Component responsible for rendering individual messages. useGetMessages: Custom hook to fetch initial messages. useSelector: Hook from react-redux to access the Redux store's state. useGetRealTimeMessage: Custom hook to handle real-time messages.

---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Message from './Message'
+import useGetMessages from '../hooks/useGetMessages';
+import { useSelector } from "react-redux";
+import useGetRealTimeMessage from '../hooks/useGetRealTimeMessage';
+
+
+
