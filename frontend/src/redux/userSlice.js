4aea0180b8c088cc6bb10bd2df50bef13db970fe
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:15:32 +0530

    Reducer Functions Reducer Functions: Inside the reducers property of createSlice, each key-value pair represents a Redux action creator function and its corresponding reducer. setAuthUser: Action creator: setAuthUser Reducer function: Updates state.authUser with action.payload, which is provided when the action is dispatched. setOtherUsers: Action creator: setOtherUsers Reducer function: Updates state.otherUsers with action.payload. setSelectedUser: Action creator: setSelectedUser Reducer function: Updates state.selectedUser with action.payload. setOnlineUsers: Action creator: setOnlineUsers Reducer function: Updates state.onlineUsers with action.payload.

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,6 +1,26 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name:"user"
+    name:"user",
+    initialState:{
+        authUser:null,
+        otherUsers:null,
+        selectedUser:null,
+        onlineUsers:null,
+    },
+    reducers:{
+        setAuthUser:(state,action)=>{
+            state.authUser = action.payload;
+        },
+        setOtherUsers:(state, action)=>{
+            state.otherUsers = action.payload;
+        },
+        setSelectedUser:(state,action)=>{
+            state.selectedUser = action.payload;
+        },
+        setOnlineUsers:(state,action)=>{
+            state.onlineUsers = action.payload;
+        }
+    }
 });
 
