39a255e19d1de2ee90fbe61ff8abdf7308d7deb7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:44:00 +0530

    Imports Signup: Imports the Signup component from './components/Signup'. './App.css': Imports the CSS file for styling the application. createBrowserRouter, RouterProvider: Imports from 'react-router-dom' for setting up routing in the application. HomePage: Imports the HomePage component from './components/HomePage'. Login: Imports the Login component from './components/Login'. useEffect, useState: Imports from 'react' for managing side effects and state in functional components. useSelector, useDispatch: Imports from 'react-redux' for accessing and dispatching actions to the Redux store. io: Imports the socket.io-client library for enabling WebSocket communication. setSocket: Imports the setSocket action creator from './redux/socketSlice' for updating the socket connection state. setOnlineUsers: Imports the setOnlineUsers action creator from './redux/userSlice' for updating the list of online users. BASE_URL: Imports the BASE_URL constant from '.' (likely containing the base URL of the backend API).

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import Signup from './components/Signup';
+import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import { useEffect, useState } from 'react';
+import {useSelector,useDispatch} from "react-redux";
+import io from "socket.io-client";
+import { setSocket } from './redux/socketSlice';
+import { setOnlineUsers } from './redux/userSlice';
+import { BASE_URL } from '.';
+
+
+
+
+
+
