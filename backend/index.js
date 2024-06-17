61ff68f5a399e84425e8c5a964a4dc849539728e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 09:02:30 +0530

    Express Setup Creating an Express Application: const app = express();: Creates an instance of the Express application. Middleware Setup Middleware Configuration: app.use(express.urlencoded({ extended: true }));:
    Parses incoming requests with urlencoded payloads. It is used for parsing form data submitted via POST requests.
    { extended: true } allows parsing of extended syntax with the qs library (default is true).
    app.use(express.json());:
    
    Parses incoming requests with JSON payloads. It allows the server to handle JSON data sent by clients in the request body.
    app.use(cookieParser());:
    
    Parses cookies attached to the client request object. It populates req.cookies with an object keyed by the cookie names.
    const corsOption = { origin: 'http://localhost:3000', credentials: true };:
    
    Defines CORS options specifying allowed origin (http://localhost:3000) and enabling credentials (credentials: true).
    app.use(cors(corsOption));:
    
    Integrates CORS middleware into the Express application using the specified options. This allows the server to respond to requests from the specified origin with appropriate CORS headers.

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,17 @@ import { app,server } from "./socket/socket.js";
 dotenv.config({});
 
  
+const PORT = process.env.PORT || 5000;
 
-
-
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(cookieParser());
+const corsOption={
+    origin:'http://localhost:3000',
+    credentials:true
+};
+app.use(cors(corsOption)); 
 
 
 
