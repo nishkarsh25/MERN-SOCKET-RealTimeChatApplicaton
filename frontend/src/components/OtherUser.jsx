6b63cec17b8a59d5f20ad69c9a938167ae2678a5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 11:28:48 +0530

    Props user: This prop represents the user data passed to the component. Redux Integration useDispatch:
    A hook to get the dispatch function from the Redux store.
    const dispatch = useDispatch();
    useSelector:
    
    A hook to access specific parts of the Redux state.
    const { selectedUser, onlineUsers } = useSelector(store => store.user);
    Destructures selectedUser and onlineUsers from the user slice of the state.

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -3,6 +3,8 @@ import { useDispatch,useSelector } from "react-redux";
 import { setSelectedUser } from '../redux/userSlice';
 
 const OtherUser = ({ user }) => {
+    const dispatch = useDispatch();
+    const {selectedUser, onlineUsers} = useSelector(store=>store.user);
     
     
     
