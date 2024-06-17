00bc0099bd50a6355784107bab4732569c98e1a5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 08:49:26 +0530

    Logout Function Exporting logout:
    The logout function is a synchronous function exported for use in routes or controllers.
    Clearing the Cookie:
    
    Clears the token cookie by setting its maxAge to 0.
    Returns a 200 status with a success message.
    Error Handling:
    
    Logs any errors that occur during the process.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,4 +74,12 @@ export const login = async (req, res) => {
         console.log(error);
     }
 }
-
+export const logout = (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "logged out successfully."
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
