0973457250814d3aab82c751348210cf067b11bd
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 08:56:18 +0530

    Router Setup Creating a Router: const router = express.Router();: Creates a new router object using express.Router(). This router object will be used to define and group related routes for message-related operations.

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { getMessage, sendMessage } from "../controllers/messageController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
-
+const router = express.Router();
 
 
 
