272ea642c11e3942b5f4fa9a9c750b5a1fafa642
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 08:56:08 +0530

    Import Statements Importing Express: import express from "express";: Imports the express library which is used to create and manage the application's routes.

---
+++ backend/routes/messageRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getMessage, sendMessage } from "../controllers/messageController.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
+
+
+
+
+
