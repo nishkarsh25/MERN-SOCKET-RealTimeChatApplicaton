5110ba58a6ac0442b109f5fb60ba30f388cea5ae
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-22 00:39:52 +0530

    Chnaged the link and deployed backend on render with npm run build and npm start:

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
-import store from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist';
+import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 let persistor = persistStore(store);
 
-export const BASE_URL="http://localhost:8080"
+export const BASE_URL =
+  "https://mern-socket-realtimechatapplicaton-2.onrender.com";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
